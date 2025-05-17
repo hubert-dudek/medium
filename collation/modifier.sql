c9d54d93c32a4aea9c7ce19ac049d62afe94c5b9
hubert-dudek <hubert-dudek>
2025-05-17 13:24:10 +0000

    202505 plan

--- collation/modifier.sql
+++ collation/modifier.sql
@@ -1,12 +1,12 @@
 -- Databricks notebook source
 -- MAGIC %md
--- MAGIC | Modifier | Description (Databricks SQL 16.2+)                                                                 |
+-- MAGIC | Modifier | Description                                                            |
 -- MAGIC |----------|-----------------------------------------------------------------------------------------------------|
 -- MAGIC | **CS**   | Case-Sensitive (default) – e.g., `'A'` ≠ `'a'`                 |
 -- MAGIC | **CI**   | Case-Insensitive – e.g., `'A'` = `'a'`                         |
 -- MAGIC | **AS**   | Accent-Sensitive (default) – e.g., `'e'` ≠ `'é'`               |
 -- MAGIC | **AI**   | Accent-Insensitive – e.g., `'e'` = `'é'`                       |
--- MAGIC | **RTRIM**| Trailing space insensitive – ignores trailing spaces (new in 16.2)  |
+-- MAGIC | **RTRIM**| Trailing space insensitive – ignores trailing spaces   |
 -- MAGIC
 
 -- COMMAND ----------
@@ -22,6 +22,7 @@ VALUES
   ('john'),
   ('John'),
   ('Peña'),
+  ('Pena'),
   ('Hello '),
   ('Hello'),
   ('apple'),
@@ -49,7 +50,7 @@ SELECT
 FROM
   my_table
 WHERE
-  name COLLATE UNICODE_CI = 'Pena';
+  name COLLATE UNICODE_AI = 'Pena';
 
 -- COMMAND ----------
 
@@ -62,24 +63,12 @@ WHERE
 
 -- COMMAND ----------
 
-CREATE OR REPLACE TABLE my_table_ci(name STRING COLLATE UNICODE_CI_AI); 
-INSERT INTO my_table_ci SELECT name FROM my_table;
-
 SELECT
-  name COLLATE UNICODE_CI_AI
-FROM
-  my_table_ci
-ORDER BY
-  name COLLATE UNICODE_CI_AI;
-
--- COMMAND ----------
-
-SELECT
-  name COLLATE cs_AI
+  name
 FROM
   my_table
 ORDER BY
-  name COLLATE cs_AI;
+  name;
 
 -- COMMAND ----------
 
@@ -88,4 +77,4 @@ SELECT
 FROM
   my_table
 ORDER BY
-  name COLLATE UNICODE_RTRIM;
+  name COLLATE UNICODE_CI_AI;
