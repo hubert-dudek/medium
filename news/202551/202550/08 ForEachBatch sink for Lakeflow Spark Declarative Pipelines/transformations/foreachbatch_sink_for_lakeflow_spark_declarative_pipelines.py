5e0103d5030fd952e35c3c88aa7390b9e6030fcf
hubert-dudek <hubert.j.dudek@outlook.com>
2025-12-16 14:09:59 +0000

    news updates

--- news/202551/202550/08 ForEachBatch sink for Lakeflow Spark Declarative Pipelines/transformations/foreachbatch_sink_for_lakeflow_spark_declarative_pipelines.py
+++ news/202551/202550/08 ForEachBatch sink for Lakeflow Spark Declarative Pipelines/transformations/foreachbatch_sink_for_lakeflow_spark_declarative_pipelines.py
@@ -1,19 +1,28 @@
 from pyspark import pipelines as dp
 
+
 @dp.foreach_batch_sink(name="audit_sink")
 def audit_sink(df, batch_id: int):
 
-
-    (df.write
-        .format("jdbc")
-        .option("databricks.connection", "my_lakebase_pg_conn")
-        .option("dbtable", "public.events_audited")
-        .save())
+    df.createOrReplaceTempView("df_view")
+    df.sparkSession.sql(
+        """
+    MERGE INTO hub.default.events_clicks AS tgt
+    USING (
+      SELECT count(1) as clicks FROM
+      df_view WHERE event_type = 'click') AS src
+    ON tgt.event_type = 'click'
+    WHEN MATCHED THEN UPDATE SET
+      tgt.clicks = tgt.clicks + src.clicks
+  """
+    )
+    return
 
 
 @dp.table(name="silver_events")
 def silver_events():
     return spark.readStream.table("hub.default.bronze_events")
+
 
 @dp.append_flow(target="audit_sink", name="events_flow")
 def events_flow():
--- news/202551/202550/08 ForEachBatch sink for Lakeflow Spark Declarative Pipelines/utilities/setup.sql
+++ news/202551/202550/08 ForEachBatch sink for Lakeflow Spark Declarative Pipelines/utilities/setup.sql
@@ -3,11 +3,16 @@ CREATE TABLE IF NOT EXISTS hub.default.bronze_events (
   event_type      STRING,
   user_id         STRING,
   event_ts        TIMESTAMP
-)
-USING DELTA;
+);
 
 INSERT INTO hub.default.bronze_events VALUES
   (1, 'page_view',  'user_001', TIMESTAMP '2025-12-15 10:00:00'),
   (2, 'click',      'user_002', TIMESTAMP '2025-12-15 10:01:10'),
   (3, 'purchase',   'user_001', TIMESTAMP '2025-12-15 10:02:45'),
-  (4, 'page_view',  'user_003', TIMESTAMP '2025-12-15 10:03:30');
+  (4, 'page_view',  'user_003', TIMESTAMP '2025-12-15 10:03:30'),
+  (5, 'click',      'user_002', TIMESTAMP '2025-12-15 10:01:10');
+
+  CREATE TABLE IF NOT EXISTS hub.default.events_clicks (
+  event_type      STRING,
+  clicks          BIGINT
+);
