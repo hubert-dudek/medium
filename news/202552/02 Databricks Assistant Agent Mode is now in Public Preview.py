dde49dfa2d950f1def45c022edd0696f8bf58c27
hubert-dudek <hubert.j.dudek@outlook.com>
2026-01-04 17:38:02 +0000

    202601

--- news/202552/02 Databricks Assistant Agent Mode is now in Public Preview.py
+++ news/202552/02 Databricks Assistant Agent Mode is now in Public Preview.py
@@ -6,3 +6,33 @@
 
 # MAGIC %md
 # MAGIC show sample of table @customer 
+
+# COMMAND ----------
+
+# DBTITLE 1,Market Segment Distribution
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Account Balance Statistics
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Geographic Distribution by Nation
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Account Balance by Market Segment
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Negative Balance Customers
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Inspect Session Variables
+
